86d2bab099e4376879c6d8b8c2444813b6905b3f
Chih-Wei Chang <bert.cwchang@gmail.com>
2016-09-17 15:23:43 -0400

    Fix god damn bug (it should be loc, not doc!).

--- QryIopNear.java
+++ QryIopNear.java
@@ -11,7 +11,6 @@ public class QryIopNear extends QryIop {
 
     private int opDistance;
     public QryIopNear(int opDistance) {
-        System.out.println("opDistance = " + opDistance);
         this.opDistance = opDistance;
     }
 
@@ -72,17 +71,20 @@ public class QryIopNear extends QryIop {
                         prevLoc = currLoc;
                     } else {
                         matchFound = false;
-                        ((QryIop) q_i).locIteratorAdvance();
                         break;
                     }
                 }
 
                 if (matchFound) {
+                    // When match, all loc iter need to be advanced as one
+                    // location can only be matched once.
                     positions.add(prevLoc);
                     for (int i=0; i<this.args.size(); i++) {
                         Qry q_i = this.args.get(i);
                         ((QryIop) q_i).locIteratorAdvance();
                     }
+                } else {
+                    ((QryIop) this.minLocQry()).locIteratorAdvance();
                 }
             }
 
@@ -100,7 +102,36 @@ public class QryIopNear extends QryIop {
 
     private boolean validDistance(int loc1, int loc2) {
         int diff = loc2 - loc1;
-        return (diff >= 0) && (diff <= this.opDistance);
+        // System.out.format("loc1=%d, loc2=%d, diff=%d\n", loc1, loc2, diff);
+        return (diff > 0) && (diff <= this.opDistance);
+    }
+
+    /**
+     *  Helper function, get the i-th args which has the minimal loc iter
+     *  location.
+     */
+    private Qry minLocQry() {
+        int idxOfMinLoc = 0;
+
+        for (int i=1; i<this.args.size(); i++) {
+            Qry q_i = this.args.get(i);
+            if (locIsGeqThan(this.args.get(idxOfMinLoc),
+                                 this.args.get(i))) {
+                idxOfMinLoc = i;
+            }
+        }
+        // System.out.format("idxOfMinLoc=%d\n", idxOfMinLoc);
+        return this.args.get(idxOfMinLoc);
+    }
+
+    /**
+     *  Helper function, check whether the current loc iter's location of q1 is
+     *  greater than q2 or not.
+     */
+    private boolean locIsGeqThan(Qry q1, Qry q2) {
+        // System.out.format("q1.loc=%d, q2.loc=%d\n", ((QryIop) q1).locIteratorGetMatch(), ((QryIop) q2).locIteratorGetMatch());
+        return (((QryIop) q1).locIteratorGetMatch()
+                >= ((QryIop) q2).locIteratorGetMatch());
     }
 
 }
